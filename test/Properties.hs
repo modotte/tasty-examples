0ac3027552ae940adcfea47d6adc9faa4c4a9476
modotte <runecroon@gmail.com>
2023-04-28 17:34:24 +0800

    Add basic prop_reverse

--- test/Properties.hs
+++ test/Properties.hs
@@ -1,10 +1,25 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Avoid reverse" #-}
 module Properties (properties) where
 
 import Hedgehog
-import Hedgehog.Gen
-import Hedgehog.Range
+import Hedgehog.Gen qualified as Gen
+import Hedgehog.Range qualified as Range
 import Test.Tasty
 import Test.Tasty.Hedgehog
 
+prop_reverse :: Property
+prop_reverse =
+  property $ do
+    xs <- forAll genAlphaList
+    reverse (reverse xs) === xs
+  where
+    genAlphaList = Gen.list (Range.linear 0 100) Gen.alpha
+
 properties :: TestTree
-properties = testGroup "Properties" []
+properties =
+  testGroup
+    "Properties"
+    [ testProperty "hedgehog tests" prop_reverse
+    ]
