5bcba2cad040ff3d9e16b55df2e7078eba1fcada
modotte <runecroon@gmail.com>
2023-04-28 16:34:07 +0800

    Add deps

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,11 @@
+import Hedgehog qualified as HH
+import Hedgehog.Gen qualified as HHG
+import Hedgehog.Range qualified as HHR
+import Test.Hspec qualified as H
 import Test.Tasty
-import Test.Tasty.Hedgehog as HH
-import Test.Tasty.Hspec as H
+import Test.Tasty.Hedgehog qualified as THH
+import Test.Tasty.Hspec (testSpec)
+import Test.Tasty.Hspec qualified as TH
 
 main :: IO ()
 main = defaultMain tests
@@ -9,7 +14,7 @@ tests :: TestTree
 tests = testGroup "Tests" [units, properties]
 
 units :: TestTree
-units = testGroup "Units" []
+units = testGroup "" []
 
 properties :: TestTree
 properties = testGroup "Properties" []
