d05ac0160ac85028fca6cd7a780418425de85c2a
modotte <runecroon@gmail.com>
2023-04-28 17:10:50 +0800

    Clean

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,13 +1,5 @@
-import Hedgehog qualified as HH
-import Hedgehog.Gen qualified as HHG
-import Hedgehog.Range qualified as HHR
 import Properties (properties)
-import Test.Hspec (Spec)
-import Test.Hspec qualified as H
 import Test.Tasty
-import Test.Tasty.Hedgehog qualified as THH
-import Test.Tasty.Hspec (testSpec)
-import Test.Tasty.Hspec qualified as TH
 import Units (units)
 
 main :: IO ()
--- test/Units.hs
+++ test/Units.hs
@@ -1,11 +1,22 @@
+{-# LANGUAGE BlockArguments #-}
+
 module Units (units) where
 
+import Control.Monad (join)
+import GHC.IO (unsafePerformIO)
 import Test.Hspec
 import Test.Tasty
 import Test.Tasty.Hspec
 
 units :: TestTree
+{-# NOINLINE units #-}
 units =
-  testGroup
-    "Units"
-    []
+  -- We shouldn't worry about use of unsafePerformIO here due to how hspec works.
+  -- See https://github.com/mitchellwrosen/tasty-hspec/issues/12#issuecomment-271333117
+  unsafePerformIO $ testSpec "Unit tests suite" suite
+
+suite :: SpecWith ()
+suite = do
+  describe "passing tests" $ do
+    it "1 must eval to 1" $ do
+      1 `shouldBe` 1
