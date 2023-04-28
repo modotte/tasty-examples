870c9d2ec497beacd07f9e20ded56de658750d90
modotte <runecroon@gmail.com>
2023-04-28 17:59:46 +0800

    Add tasty-rerun

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,15 @@
 import Properties (properties)
 import Test.Tasty
+import Test.Tasty.Ingredients.Basic
+import Test.Tasty.Ingredients.Rerun
 import Test.Tasty.Runners.Html
 import Units (units)
 
 main :: IO ()
-main = defaultMainWithIngredients (htmlRunner : defaultIngredients) tests
+main =
+  defaultMainWithIngredients
+    (htmlRunner : rerunningTests [listingTests, consoleTestReporter] : defaultIngredients)
+    tests
 
 tests :: TestTree
 tests = testGroup "Tests" [units, properties]
